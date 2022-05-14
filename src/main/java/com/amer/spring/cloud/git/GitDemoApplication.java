2e350490a4ff4192c2c6eeaeddf97146b49c9b32
mohamed-amer152 <rezk2562@gmail.com>
2022-05-14 14:49:20 +0400

    added the git demmo project

---
+++ src/main/java/com/amer/spring/cloud/git/GitDemoApplication.java
@@ -0,0 +1,23 @@
+package com.amer.spring.cloud.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitDemoApplication {
+
+	@GetMapping("/")
+	public String hellow()
+	{
+		return " hello from first git " ;
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+}
